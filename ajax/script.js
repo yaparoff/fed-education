433fec3b82c89081cd8db76d8da248d5a2307e73
Andrei Iaparov <andrei_iaparov@epam.com>
2017-06-06 10:40:25 +0400

    function trim is added

--- ajax/script.js
+++ ajax/script.js
@@ -24,22 +24,35 @@ function showUserInfo(user) {
 showUserInfo('yaparoff');
 
 function showRepositoryInfo(user, repo) {
-    var xhr = new XMLHttpRequest();
-    var path = 'https://api.github.com/repos/' + user + '/' + repo;
-    xhr.open('GET', path, true);
-    xhr.send();
-    xhr.onreadystatechange = function() {
-    if (xhr.readyState != 4) return;
-        if (xhr.status != 200) {
-            alert( xhr.status + ': ' + xhr.statusText );
-        } else {
-            var obj = JSON.parse(xhr.responseText);
-            repoName.innerHTML = obj.name;
-            issuesCount.innerHTML = obj.open_issues_count;
-        }
-    } 
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        var path = 'https://api.github.com/repos/' + user + '/' + repo;
+        xhr.open('GET', path, true);
+        
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState != 4) return;
+            if (xhr.status != 200) {
+                alert( xhr.status + ': ' + xhr.statusText );
+            } else {
+                var obj = JSON.parse(xhr.responseText);
+                repoName.innerHTML = obj.name;
+                issuesCount.innerHTML = obj.open_issues_count;
+            }
+        };
+        xhr.onerror = function() {
+            reject(new Error("Error"));
+        };
+        xhr.send();
+    });
+    
+    
 }
-showRepositoryInfo('yaparoff', 'fed-education');
+showRepositoryInfo('yaparoff', 'fed-education')
+    .then( 
+        response => alert(`Fulfilled: ${response}`), 
+        error => alert(`Rejected: ${error}`) 
+
+    );
 
 
 var submit = document.querySelector('.form__submit');
@@ -52,12 +65,12 @@ submit.addEventListener('click', function(event) {
 });
 
 function createIssue(user, repo) {
-    if ((issueTitle.value == '') || (issueTitle.value.search(/\s/g) > -1)) {
+    if (issueTitle.value.trim == '') {
         alert('Error! Issue title is empty!');
     } else {
         console.log(123);
         //var token = '5b6d480daf6e887060d0d71fa107f3db0a8ffc13';
-        var token = '6733b08a3ef65e0dc9a1dcab2a2dc968fa0afa67';
+        var token = '47e1fec151d38d2f740e20d116e84e4db62c265b';
         params = {
             "title": issueTitle.value,
             "body": issueBody.value
@@ -68,7 +81,11 @@ function createIssue(user, repo) {
         xhr.setRequestHeader('Authorization', 'token ' + token);
         xhr.send(JSON.stringify(params));
         xhr.onreadystatechange = function() {
-            showRepositoryInfo(user, repo);
+            showRepositoryInfo(user, repo)
+                .then( 
+                    response => alert(`Fulfilled: ${response}`), 
+                    error => alert(`Rejected: ${error}`) 
+                );
         }
     }
 }
